65861557dc64c628e2b9e4ecd9d5e1977c7f0c0d
Leonardo Leoncio Bravo Ricapa <u20211c688@upc.edu.pe>
2025-04-16 06:22:22 -0500

    Initial commit

---
+++ src/main/java/com/example/financelloapi/FinancelloApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.financelloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancelloApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancelloApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/financelloapi/FinancelloApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financelloapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancelloApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
