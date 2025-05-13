c1d36aa87c6c10597c80d48d9dbc7c2b2bd95403
Mauricio Vera Castellon <mcwanted123@gmail.com>
2025-05-13 14:16:08 -0500

    First commit of the feature monthly budget

---
+++ src/main/java/com/example/financelloapi/repository/BudgetRepository.java
@@ -0,0 +1,10 @@
+package com.example.financelloapi.repository;
+
+import com.example.financelloapi.model.entity.Budget;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BudgetRepository extends JpaRepository<Budget, Integer> {
+    Optional<Budget> findByUser_IdAndPeriod(Integer userId, String period);
+}
