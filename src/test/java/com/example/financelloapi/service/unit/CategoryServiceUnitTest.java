c2256f9cdd3b7ad2c1eac23f5f2a9f46adad43a4
nat <nathaly.elianev@gmail.com>
2025-06-03 20:18:36 -0500

    test: add unit test for valid category creation

--- src/test/java/com/example/financelloapi/service/unit/CategoryServiceUnitTest.java
+++ src/test/java/com/example/financelloapi/service/unit/CategoryServiceUnitTest.java
@@ -1,14 +1,24 @@
 package com.example.financelloapi.service.unit;
 
+import com.example.financelloapi.dto.request.CategoryRequest;
+import com.example.financelloapi.dto.test.CategoryResponse;
 import com.example.financelloapi.mapper.CategoryMapper;
+import com.example.financelloapi.model.entity.Category;
+import com.example.financelloapi.model.entity.User;
 import com.example.financelloapi.repository.CategoryRepository;
 import com.example.financelloapi.repository.FinancialMovementRepository;
 import com.example.financelloapi.repository.UserRepository;
 import com.example.financelloapi.service.CategoryService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 public class CategoryServiceUnitTest {
 
@@ -23,5 +33,25 @@ public class CategoryServiceUnitTest {
         MockitoAnnotations.initMocks(this);
     }
 
+    @Test
+    @DisplayName("CP01 - Crear categoría valida")
+    void createCategory_valid_returnCreated() {
+        Integer userId = 1;
+        CategoryRequest request = new CategoryRequest("Ocio", "Gastos en actividades de ocio");
+        Category entity = new Category();
+        User user = new User();
+        user.setId(userId);
+        CategoryResponse response = new CategoryResponse("Ocio", "Gastos en actividades de ocio");
 
+        when(categoryRepository.existsByNameAndUserId(request.name(), userId)).thenReturn(false);
+        when(userRepository.findByIdCustom(userId)).thenReturn(Optional.of(user));
+        when(categoryMapper.toCategoryEntity(request, user)).thenReturn(entity);
+        when(categoryRepository.save(entity)).thenReturn(entity);
+        when(categoryMapper.toCategoryResponse(entity)).thenReturn(response);
+
+        CategoryResponse result = categoryService.createCategory(userId, request);
+
+        assertEquals("Ocio", result.name());
+        assertNotNull(result);
+    }
 }
