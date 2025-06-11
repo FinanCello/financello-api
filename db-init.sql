fcfd38a0e826363d825a5f941fd747e1dff4995e
LiamQuinoNeff <liamquinoneff@gmail.com>
2025-06-10 21:04:09 -0500

    Fix: Limpieza de código y actualización de validaciones en Postman

--- db-init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS roles (
-                                     role_id SERIAL PRIMARY KEY,
-                                     role_type VARCHAR(255) NOT NULL UNIQUE
-);
-
-INSERT INTO roles (role_id, role_type) VALUES
-                                           (1, 'ADMIN'),
-                                           (2, 'BASIC')
-ON CONFLICT (role_id, role_type) DO NOTHING;
