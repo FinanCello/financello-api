a108de2d7171630f9647efbed9ba23494d061ad6
ALX-13 <leonardobravoricapa@gmail.com>
2025-06-24 17:01:10 -0500

    Fix: AuthResponse, now give Id, it's important for front-end

--- src/main/java/com/example/financelloapi/dto/test/AuthResponse.java
+++ src/main/java/com/example/financelloapi/dto/test/AuthResponse.java
@@ -2,5 +2,5 @@ package com.example.financelloapi.dto.test;
 
 import com.example.financelloapi.model.enums.UserType;
 
-public record AuthResponse(String email, String firstName, String lastName, UserType userType, String  token) {
+public record AuthResponse(Integer id, String email, String firstName, String lastName, UserType userType, String token) {
 }
